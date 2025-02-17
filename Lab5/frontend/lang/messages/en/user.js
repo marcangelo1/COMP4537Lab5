38bd620eef700c858ff085c30619fe1f67ac0a4f
Marc <144364703+marcangelo1@users.noreply.github.com>
2025-02-16 19:41:57 -0800

    Change productionendpoint

--- Lab5/frontend/lang/messages/en/user.js
+++ Lab5/frontend/lang/messages/en/user.js
@@ -1,6 +1,6 @@
 const localEndpoint = "http://localhost:8080/lab5/api/v1/sql/";
 const productionEndpoint =
-  "https://comp4537lab5-r6u6.onrender.com/lab5/api/v1/sql/";
+  "https://comp4537lab5-1-cw83.onrender.com//lab5/api/v1/sql/";
 const failedPostResponseMsg =
   "Error: No response from server. Please try again.";
 const invalidSqlQuery =
